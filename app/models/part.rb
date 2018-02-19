9d528c5abf4023d4103a780bdb761b8d117fddeb
Papo Killer <papokiller@protonmail.com>
2018-02-18 20:29:25 -0400

    fix

--- app/models/part.rb
+++ app/models/part.rb
@@ -4,6 +4,15 @@ class Part < ApplicationRecord
   belongs_to :episode
   before_create :download_video
 
+  def video_url
+    if status > 0
+      uploader = uploader_files.last
+      uploader_data = uploader[:data]
+      return "https://raw.githubusercontent.com/#{uploader.uploader.username}/#{uploader_data['repo']}/master#{video_url_local}"
+    end
+    super
+  end
+
   private
   def download_video
   	File.open(Rails.root.join('public', uid), "w") do |f|
